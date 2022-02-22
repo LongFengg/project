56afef076faf46fddd4f75b7d0091554268bce0d
Vlad <shchohalev@gmail.com>
2022-02-22 10:54:51 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+"use sctrict";
+
+// let number = 4.6;
+
+// console.log(-4/0);
+// console.log('string' * 9);
+
+// const persone = `5`;
+
+// const bool = true;
+
+// console.log(something);
+
+// let und;
+// console.log(und);
+
+// const obj = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
+// };
+
+// console.log(obj.name);
+
+// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
+// console.log(arr[0]);
+
