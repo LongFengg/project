341b6fcde6a645817f0bd771eeca6139c692508c
Vlad <shchohalev@gmail.com>
2022-02-22 11:03:50 +0300

    second

--- js/script.js
+++ js/script.js
@@ -1,27 +1,12 @@
 "use sctrict";
 
-// let number = 4.6;
+let incr = 10,
+    decr= 10;
 
-// console.log(-4/0);
-// console.log('string' * 9);
+incr++;
+decr--;
 
-// const persone = `5`;
+const isChecked = true,
+      isClose = false;
 
-// const bool = true;
-
-// console.log(something);
-
-// let und;
-// console.log(und);
-
-// const obj = {
-//     name: "John",
-//     age: 25,
-//     isMarried: false
-// };
-
-// console.log(obj.name);
-
-// let arr = ['plum.png', 'orange.jpg', 'apple.bmp'];
-// console.log(arr[0]);
-
+    console.log(isChecked || !isClose);
