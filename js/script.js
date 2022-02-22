5d72d65c98e9867b390dde2cc5aa4b791151ad3b
unknown <User@MAPID98.mapid.local>
2022-02-22 16:47:45 +0300

    14

--- js/script.js
+++ js/script.js
@@ -1,34 +1,22 @@
 "use sctrict";
 
-if (4 == 4) {
-    console.log('Ok!');
-}
+let num = 50;
 
-
-
-// if (num < 49){
-//     console.log('Error');
-// } else if(num>100){
-//     console.log('To much!');
-// } else{
-//     console.log('Ok');
+// while (num <= 55) {
+//     console.log(num);
+//     num++;
 // }
 
-// (num === 50) ? console.log('Ok') : console.log('Error');
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while(num < 55);
 
-const num = 50;
-
-switch (num){
-    case 49:
-        console.log('Неверно');
-        break;
-    case 50:
-        console.log('Верно');
-        break;
-    case 100:
-        console.log('Неверно');
-        break;
-    default:
-        console.log('Не в этот раз!');
-        break;
+for (let i = 1; i < 8; i++){
+    if (i===6){
+        // break;
+        continue;
+    } 
+    console.log(i);
 }
