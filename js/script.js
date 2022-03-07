a4b6d0fbbedbc71df83444cf565a26a1c8333ec7
Vladislav Schogolev <shchohalev@gmail.com>
2022-03-07 13:43:33 +0300

    Praktika18

--- js/script.js
+++ js/script.js
@@ -1,28 +1,78 @@
 "use sctrict";
 
-const str = 'teSt';
-const arr = [1, 2, 4];
+let numberOfFilms;
 
-// console.log(str[2]);
+function start() {
+    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// console.log(str.toUpperCase());
+    while(numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+    }
+}
 
-console.log(str.toLowerCase());
-console.log(str);
+start();
 
-const fruit = 'Some fruit';
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-console.log(fruit.indexOf('fruit'));
+function rememberMyFilms() {
+    for (let i = 0; i < 2; i++){
+        const a = prompt('Один из последних просмотренных фильмов?', ''),
+              b = prompt('На сколько его оцените?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--;
+        }
+    }
+}
 
-const logg = 'Hello world';
+// rememberMyFilms();
 
-// console.log(logg.slice(6));
+function detectPersonalLevel(){
+    if (personalMovieDB.count < 10) {
+        console.log('Просмотрено довольно мало фильмов');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+        console.log('Вы классический зритель');
+    } else if (personalMovieDB.count >= 30) {
+        console.log('Вы киноман');
+    } else {
+        console.log('Произошла ошибка');
+    }
+}
 
-console.log(logg.substr(6, 5));
+// detectPersonalLevel();
 
-const num = 12.2;
-console.log(Math.round(num));
+function writeYoutGenres(){
+    for (let i = 1; i <= 3; i++) {
+    const a = prompt('Ваш любимый жанр под номером ' + i, '');
 
-const test = '12.2px';
-console.log(parseInt(test));
-console.log(parseFloat(test));
+    if (a != null && a != '' && a.length < 50) {
+        personalMovieDB.genres[i-1] = a;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--;
+    }
+    }
+}
+
+writeYoutGenres();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.privat);
+
+
