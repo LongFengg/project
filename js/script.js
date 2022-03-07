5b57fcf3dcd50d3ef1921011689b5bbe345b5af2
Vlad Shchohalev <shchohalev@gmail.com>
2022-03-07 12:23:54 +0300

    17

--- js/script.js
+++ js/script.js
@@ -1,34 +1,28 @@
 "use sctrict";
 
-let num = 10;
+const str = 'teSt';
+const arr = [1, 2, 4];
 
-function showFirstMessage(text) {
-    console.log(text);
-    num = 25;
-}
+// console.log(str[2]);
 
-showFirstMessage("Hello world!");
-console.log(num);
+// console.log(str.toUpperCase());
 
-// function calc(a, b){
-//     return (a + b);
-// }
+console.log(str.toLowerCase());
+console.log(str);
 
-// console.log(calc(4, 7));
+const fruit = 'Some fruit';
 
-function ret() {
-    let num = 50;
-    return num;
-}
+console.log(fruit.indexOf('fruit'));
 
-const antoherNum = ret();
-console.log(antoherNum);
+const logg = 'Hello world';
 
-const logger = function(){
-    console.log('Hello');
-};
+// console.log(logg.slice(6));
 
-logger();
+console.log(logg.substr(6, 5));
 
-const calc = (a, b) => a + b;
-console.log(calc(5,4));
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = '12.2px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
